1bbc10cc6f0916f50b96f64580d6ada859ecbfa2
jolly01008 <jollgmll292@gmail.com>
2023-04-20 00:23:05 +0800

    feat: create views folder and set express-handlebars

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const exphbs = require("express-handlebars");
 
-app.get('/',(req,res)=>{
-  res.send('Login system TEST')
+app.engine("handlebars",exphbs({defaultLayout: "main"}))
+app.set("view engine","handlebars")
+
+app.get('/',(req,res) => {
+  res.render('index')
 })
 
-app.listen('3000',()=>{
+
+app.listen('3000', () => {
   console.log(`App is running on http://localhost:${port}`)
 })
 
