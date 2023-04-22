c7bf774f1512016b647d3c00312a3980233aaf34
jolly01008 <jollgmll292@gmail.com>
2023-04-22 14:33:00 +0800

    feat: check user info

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const exphbs = require("express-handlebars");
+const exphbs = require("express-handlebars")
+const Users = require("./models/Users")
 
 app.engine("handlebars",exphbs({defaultLayout: "main"}))
 app.set("view engine","handlebars")
+app.use(express.urlencoded({ extended: true })) 
 
 app.get('/',(req,res) => {
   res.render('index')
 })
 
+app.post('/',(req,res) => {
+  const email = req.body.email
+  const userPassword = req.body.password
+  const error = "Your Email is fail."
+  console.log(req.body , email , userPassword)
+  Users.findOne({ email })
+    .lean()
+    .then( data => {
+      if(data === null ){
+        return res.render('index',{error , email})
+      }
+      if(data.password === userPassword ){
+        return res.render('welcome',{name: data.firstName})
+      }
+      else{
+        return res.render('index',{error,email})
+      }
+    })
+})
 
 app.listen('3000', () => {
   console.log(`App is running on http://localhost:${port}`)
