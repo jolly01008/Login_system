373e666d4bc4a2eda75c43e10a6d206e8ecb717f
jolly01008 <jollgmll292@gmail.com>
2023-05-02 12:28:33 +0800

    feat: add cookie-parser and cookies

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require("express-handlebars")
+const cookieParser = require("cookie-parser")
 
 require('./config/mongoose') // 載入mongoose
 const routes = require('./routes')
@@ -9,6 +10,7 @@ const routes = require('./routes')
 app.engine("handlebars",exphbs({defaultLayout: "main"}))
 app.set("view engine","handlebars")
 app.use(express.urlencoded({ extended: true })) 
+app.use(cookieParser())
 
 app.use(routes) //將request 導入路由器
 
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,6 +4,12 @@ const Users = require("../../models/Users")
 
 
 router.get('/',(req,res) => {
+  const usercookie = req.cookies.userName
+  //判斷是否有req.cookies.userName，若有就顯示已經登入，不需再登入一次
+  if(usercookie){
+    console.log(usercookie)
+    return res.send(`Hi，${usercookie}，您已經登入`)
+  }
   res.render('index')
 })
 
@@ -14,7 +20,8 @@ router.post('/',(req,res) => {
   .lean()
   .then( data => {
     if(data){
-      return res.render('welcome',{ name: data.firstName })
+      res.cookie('userName',data.firstName)  //如果登入成功就set cookie
+      res.render('welcome',{ name: data.firstName })
     } else{
       const errorMessage = `Wrong email or password, please try again.`
       res.render('index', {errorMessage , email:userEmail})
